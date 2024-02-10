c38f9c2a2621c7f370611861cb39155fa7345dae
Amulya Kumar Maurya <akmewtriple7@gmail.com>
2024-02-11 02:18:56 +0530

    created 01basics/05_strings.js

---
+++ 01basics/05_strings.js
@@ -0,0 +1,15 @@
+const name="bruce";
+const greeting="Good Morning";
+
+console.log(greeting + " " + name);
+
+console.log(`Hello my name is ${name} and I am ${30} years old`);
+
+const gameName0="Death Stranding";  //creating a string in java script
+const gameName= new String("Death Stranding");  //creating a string object in java script
+console.log(gameName0);
+console.log(gameName);
+
+console.log(gameName.length); //15
+console.log(gameName[0]); //D
+console.log(gameName.charAt(4));//h starting index from 0
