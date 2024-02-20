b62c5588273d9b81fb2a04d9db0b1ba2264b1636
Amulya Kumar Maurya <akmewtriple7@gmail.com>
2024-02-20 23:08:23 +0530

    //updated 04_iife.js

--- 03basics/04_iife.js
+++ 03basics/04_iife.js
@@ -15,3 +15,14 @@
 
 
 //()(); //this semicolon is necessary to end the iife function
+
+//the iife function is called immediately after the declaration and can take arguments also
+// (function(x,y){console.log(x+y);}(5,6));//11
+
+//another way of passing argument in iife
+var add=function(x,y){return x+y;};//this is a function expression
+add(3,4);//7
+
+//closure
+//when a function has access to variables that are declared outside of it
+//these functions are said to have access to outer scopes or they maintain access to those variables even when they go out of their scope 
