abfc061795ec0f8dccf3e2a22d3021e5e9dc93ba
Amulya Kumar Maurya <akmewtriple7@gmail.com>
2024-02-22 03:36:59 +0530

    // 05Iterations/one.js

---
+++ 05Iterations/one.js
@@ -0,0 +1,33 @@
+//for
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if(element==5){
+//         console.log(' 5 is best number');
+//     }
+//     console.log(element);
+// }
+
+//print tables from 1 to 10 using for loop
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Table of ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// }
+
+//break and continue
+for (let i = 0; i < 10; i++) {
+    if(i==5){
+        continue;
+    }
+    console.log(i);
+}
+console.log("Outside the loop");
+
+for (let i = 0; i < 20; i++) {
+    if(i==5){
+        break;
+    }
+    console.log(i);
+}
+console.log("Outside the loop");
