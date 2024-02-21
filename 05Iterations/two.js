df58b2dbb162b1faa22dfbe78a7e83eb94d6ca79
Amulya Kumar Maurya <akmewtriple7@gmail.com>
2024-02-22 03:43:26 +0530

    // 05Iterations/two.js

---
+++ 05Iterations/two.js
@@ -0,0 +1,29 @@
+//while loop
+
+// while (condition) {
+    
+// }
+
+// let index=0
+// while (index<10) {
+//     console.log(index);
+//     index=index+2;   
+// }
+
+let myheros=["superman","batman","wonderwoman","flash","aquaman"]
+let i=0
+while (i<myheros.length) {
+    console.log(myheros[i]);
+    i++;
+}
+
+//do while loop
+// do {
+    
+// } while (condition);
+
+let index=11
+do {
+    console.log(index);
+    index=index+2;   
+} while (index<10);
