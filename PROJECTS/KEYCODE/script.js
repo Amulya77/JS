7e3e00c2d20ac366da72f8b3b009cdef3eee66ad
Amulya Kumar Maurya <akmewtriple7@gmail.com>
2024-02-26 22:20:09 +0530

    // PROJECTS/KEYCODE

---
+++ PROJECTS/KEYCODE/script.js
@@ -0,0 +1,19 @@
+const insert=document.getElementById('insert');//get the element with id insert
+
+window.addEventListener('keydown',(e)=>{//add event listener to window for keydown event
+    insert.innerHTML=`
+    <div class='color'>
+  <table>
+  <tr>
+    <th>Key</th>
+    <th>Keycode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key === ' ' ? 'Space' : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+  </div>`;//insert the key, keycode and code into the insert element as innerHTML
+});
