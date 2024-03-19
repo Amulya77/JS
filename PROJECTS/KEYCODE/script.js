e11698f962bbaa6a3ced47c6db1dc3d2da0e1c3e
Amulya Kumar Maurya <akmewtriple7@gmail.com>
2024-03-19 22:00:49 +0530

    Updated Keycode-code

--- PROJECTS/KEYCODE/script.js
+++ PROJECTS/KEYCODE/script.js
@@ -10,10 +10,12 @@ window.addEventListener('keydown',(e)=>{//add event listener to window for keydo
     <th>Code</th>
   </tr>
   <tr>
-    <td>${e.key === ' ' ? 'Space' : e.key}</td>
+    <td>${e.key === ' ' ? 'Space' : e.key}</td>  
     <td>${e.keyCode}</td>
     <td>${e.code}</td>
   </tr>
-</table>
+</table>//
   </div>`;//insert the key, keycode and code into the insert element as innerHTML
 });
+
+//keydown is an event that fires when a key is pressed down
